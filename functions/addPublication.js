1ae7a2e0d4de2b2602eeec07a3f11311d006eba9
Javi Guignard <javierguignard@gmail.com>
2024-01-25 16:06:29 -0300

    initial commit

---
+++ functions/addPublication.js
@@ -0,0 +1,43 @@
+// This function is the endpoint's request handler.
+exports = function(payload, response) {
+    // Data can be extracted from the request as follows:
+    const responseData = {};
+    const user_id = payload.headers["x-us"] || 0;
+    if (user_id === 0){
+        responseData.error = 'Not Authorized'
+        response.setStatusCode(401);
+        response.setBody(JSON.stringify(responseData));
+        return response
+    }
+    // Raw request body (if the client sent one).
+    // This is a binary object that can be accessed as a string using .text()
+    const {course, material, object_type, is_question, content, parent_id} = payload.body;
+
+    const newComment = {
+      user_id: user_id,
+      is_question: true ? is_question : false, // tipo de contribución, p.ej., "comment"
+      active: true, // estado activo del comentario
+      content: content, // contenido del comentario
+      answers: [], // lista inicial de respuestas (vacía)
+      starred_by: [user_id], // lista inicial de "likes" (vacía)
+      created_at: new Date(), // fecha de creación
+      updated_at: new Date() // fecha de última actualización
+    };
+
+    const db = context.services.get("AgComments").db("material_contributions");
+    const doc = db.collection(`contributions_${course}_${material}`);
+    has_parent = parent_id ? true : false
+    if (has_parent){
+        //find parent and insert new answer on answer array
+        doc.updateOne({_id: BSON.ObjectId(parent_id)}, {$push: {answers: newComment}})
+    }else{
+        doc.insertOne(newComment)
+    }
+
+
+
+    // Establecer la respuesta
+    response.setStatusCode(200); // Código de estado HTTP para "OK"
+    response.setBody(JSON.stringify(responseData));
+    return response
+};
---
+++ functions/listPublications.js
@@ -0,0 +1,71 @@
+// This function is the endpoint's request handler.
+exports = async function(payload, response) {
+    // Data can be extracted from the request as follows:
+    const user_id = payload.headers["x-UserId"] || 0;
+
+    // Raw request body (if the client sent one).
+    // This is a binary object that can be accessed as a string using .text()
+    const {filter, course, material, page, pager} = payload.query;
+
+
+    const db = context.services.get("AgComments").db("material_contributions");
+    const collection = db.collection(`contributions_${course}_${material}`);
+  
+    
+    // Parámetros de paginación
+    const limit = parseInt(pager) || 10; // Número fijo de documentos por página
+    const skip = (parseInt(page) - 1) * parseInt(limit) || 0;
+
+    // Crear una consulta con ordenación y paginación
+    const query = {};
+    let sort = {};
+    switch (filter) {
+        case "top":
+            sort = { "starredByLength": -1 }; // Ordenar por los más votados (mayor longitud de starred_by)
+            break;
+        case "unanswered":
+            sort = { "answersLength": 1 }; // Ordenar por los que tienen menos respuestas (menor longitud de answers)
+            break;
+        default:
+            sort = { "created_at": -1 }; // Orden por defecto
+    }
+
+    // Ejecutar la consulta
+    const totalPages = await collection.count(query) / limit;
+    
+    
+    let pipeline = [
+        {
+            $addFields: {
+                answersLength: { $size: "$answers" },
+                starredByLength: { $size: "$starred_by" }
+            }
+        },
+        // Ordenar basado en el filtro
+        { $sort: sort },
+        // Paginación
+        { $skip: skip },
+        { $limit: limit }
+    ];
+
+    // Ejecutar la consulta de agregación
+    const comments = await collection.aggregate(pipeline).toArray();
+    
+    //const comments = await collection.find(query).sort(sort).skip(skip).limit(limit).toArray();
+
+    // Preparar la respuesta
+    const responseData = {
+        data: comments,
+        metadata: {
+            totalPages: totalPages,
+            current_page: page,
+            current_limit: limit
+        }
+    };
+
+    // Establecer la respuesta
+    response.setStatusCode(200); // Código de estado HTTP para "OK"
+    response.setBody(JSON.stringify(responseData));
+    return response
+    
+};
